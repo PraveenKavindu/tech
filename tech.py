ca06df9160f02432a4a199c0b8c6a10dca21aa43
CRAZY HACKER <52315456+PraveenKavindu@users.noreply.github.com>
2021-05-14 18:45:09 +0530

    Update tech.py

--- tech.py
+++ tech.py
@@ -1,21 +1,4 @@
-# Python code to demonstrate
-# length of list
-# using naive method
-
-# Initializing list
-test_list = [ 1, 4, 5, 7, 8 ]
-
-# Printing test_list
-print ("The list is : " + str(test_list))
-
-# Finding length of list
-# using loop
-# Initializing counter
-counter = 0
-for i in test_list:
-	
-	# incrementing counter
-	counter = counter + 1
-
-# Printing length of list
-print ("Length of list using naive method is : " + str(counter))
+string1 = "Linux"
+string2 = "Hint"
+joined_string = string1 + string2
+print(joined_string)
